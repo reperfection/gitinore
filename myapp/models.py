c4870ab92bb74fe14e0d1861cacf39b716b8afc2
reperfection <wodhkszxc@gmail.com>
2022-07-19 00:08:53 +0900

    models.py 작성

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('data published')
+    body = models.TextField()
+    image = models.ImageField(upload_to = 'images/', blank = True)
+    
+    def __str__(self):
+        return self.title
