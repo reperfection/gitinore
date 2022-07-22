304bd322da61a792a208b15ea025d476b04046c3
reperfection <wodhkszxc@gmail.com>
2022-07-22 20:06:18 +0900

    commentform, hashtagform 추가

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,7 +1,17 @@
 from django import forms
-from .models import Blog
+from .models import Blog, Comment, HashTag
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ['title', 'body', 'image']
+        fields = ['title', 'body', 'image', 'hashtags']
+        
+class CommentForm(forms.ModelForm) :
+    class Meta :
+        model = Comment
+        fields = ['text']
+        
+class HashTagForm(forms.ModelForm) :
+    class Meta :
+        model = HashTag
+        fields = ['name']
