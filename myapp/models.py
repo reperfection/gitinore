458021c2ede83df7c25f4f47be9e4cb8d9b9f17f
reperfection <wodhkszxc@gmail.com>
2022-07-22 20:06:38 +0900

    comment, hashtag 추가

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,20 @@ class Blog(models.Model):
     pub_date = models.DateTimeField('data published')
     body = models.TextField()
     image = models.ImageField(upload_to = 'images/', blank = True)
-    
+    hashtags = models.ManyToManyField('HashTag', blank = True)
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    def __str__(self):
+        return self.text
+        
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    text = models.CharField(max_length=50)
+
+class HashTag(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+    
